44422d5f4eafb6f472c0823a0ecd93ec358117b2
Alvaro <alvaro.exposito.128@iesoretania.es>
2022-10-19 08:47:05 +0200

    "Subido proyecto nuevo Añadir contacto"

---
+++ contacto.php
@@ -0,0 +1,16 @@
+<?php
+
+
+    if($_POST){
+        $nombre = $_POST['nombre'];
+        $apellido = $_POST['apellido'];
+        $edad = $_POST['edad'];
+        
+        echo "<p>Nombre: " . $nombre . "</p>";
+        echo "<p>Apellido: " . $apellido . "</p>";
+        echo "<p>Edad " . $edad . "</p>";
+    }
+
+
+
+?>
